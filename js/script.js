4652925c577b043a3cbca6d9aeffcebb248f8100
Ahmed Bein <98428365+AhmedAbdulwasi@users.noreply.github.com>
2024-05-02 13:42:59 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 var quotes_mov= [
     "I'm only starting, watch me grow.",
     "Learning is what keeps my mind going.",
-    "Give me a chance to Learn.",
+    "Give me a chance to learn.",
     "I love Python.",
+    "And Java.",
     "A positive environment is what makes a company great. It\'s something that I would love to be in.",
     "Challenge me, and I will succeed.",
     "Mistakes are part of the learning process.",
     "I love to learn new things. This makes me adapt quicker to new environments.",
-    "I aspire to be a Software Devleoper/Engineer."
+    "I aspire to be a Software Developer/Engineer."
 
 ]
 
